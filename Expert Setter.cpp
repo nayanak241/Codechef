1c2f97583a62b8b8d2833faba80ac0caf8cac8da
nayanak241 <157300482+nayanak241@users.noreply.github.com>
2024-11-10 19:08:10 +0530

    Create Expert Setter.cpp
    https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/EXPERT

---
+++ Expert Setter.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    double x,y;
+    cin>>x>>y;
+double p = x/2;
+    if (p<=y)cout<<"YES"<<endl;
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}
+}

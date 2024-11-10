da2fc0f577abddf054f2725b3c6be052525f620b
nayanak241 <157300482+nayanak241@users.noreply.github.com>
2024-11-10 19:16:38 +0530

    Create Good program.cpp
    https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/NIBBLE

---
+++ Good program.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int x;
+    cin>>x;
+    if(x%4==0)cout<<"GOOD"<<endl;
+    else{
+        cout<<"NOT GOOD"<<endl;
+    }
+}
+}

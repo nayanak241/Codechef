5cc89e2127d74f3577e62742f682c61c328766ac
nayanak241 <157300482+nayanak241@users.noreply.github.com>
2024-11-10 19:15:48 +0530

    Create Sasta Shark tank.cpp
    https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SST

---
+++ Sasta Shark tank.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a,b;
+    cin>>a>>b;
+    if(a*10 >(b*10)/2)cout<<"FIRST"<<endl;
+    else if(a*10<(b*10)/2)cout<<"SECOND"<<endl;
+    else{
+        cout<<"ANY"<<endl;
+    }
+}
+}

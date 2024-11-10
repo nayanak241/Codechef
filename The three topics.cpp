1da5d14323ac4217375c92511e6643d25259f315
nayanak241 <157300482+nayanak241@users.noreply.github.com>
2024-11-10 19:13:25 +0530

    Create The three topics.cpp
    https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/THREETOPICS?tab=statement

---
+++ The three topics.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int a,b,c,x;
+cin>>a>>b>>c>>x;
+if(x==a || x==b||x==c)cout<<"YES"<<endl;
+else{
+    cout<<"NO"<<endl;
+}
+}

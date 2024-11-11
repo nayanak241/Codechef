7131bcd8b38dc084bc0e2fd964b6027eb5c38bcf
nayanak241 <157300482+nayanak241@users.noreply.github.com>
2024-11-11 23:12:29 +0530

    Create Watching Movies at 2x.cpp
    https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MOVIE2X

---
+++ Watching Movies at 2x.cpp
@@ -0,0 +1,13 @@
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MOVIE2X
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int x,y;
+cin>>x>>y;
+int p = y/2;
+int q = x-y;
+int r = q+p;
+cout<<r<<endl;
+}

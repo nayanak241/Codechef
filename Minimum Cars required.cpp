0afe8d2f1205dde0b605b78ac27b72f98a508b1e
nayanak241 <157300482+nayanak241@users.noreply.github.com>
2024-11-11 23:04:53 +0530

    Create Minimum Cars required.cpp
    https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MINCARS

---
+++ Minimum Cars required.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    if(n<=4)cout<<"1"<<endl;
+   else if(n%4==0)cout<<n/4<<endl;
+   else{
+       cout<<(n/4)+1<<endl;
+   }
+    
+}
+return 0;
+}

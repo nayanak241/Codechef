ebaed54dacca383263d4f37d7c30c8ce26732fee
nayanak241 <157300482+nayanak241@users.noreply.github.com>
2024-11-22 11:41:40 +0530

    Create Moneymaking.cpp
    https://www.codechef.com/problems/NUGGET

---
+++ Moneymaking.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int x, y;
+    cin >> x >> y;
+    int result = (x * 5000) + (y * 9800);
+    cout << result << endl;
+    return 0;
+}

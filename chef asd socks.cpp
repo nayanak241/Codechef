9a8de92d1c12eb271137b7b9ac3dec05754d5ed8
nayanak241 <157300482+nayanak241@users.noreply.github.com>
2024-12-04 22:43:08 +0530

    Create chef asd socks.cpp
    https://www.codechef.com/problems/CHEFSOCKS

---
+++ chef asd socks.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int a, x, y;
+    cin >> a >> x >> y;
+    if (x + y >= a) cout << "yes" << endl;
+    else {
+        cout << "no" << endl;
+    }
+    return 0;
+}

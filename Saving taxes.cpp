35d01860eb6c765ade75971a0e931df8c26f2425
nayanak241 <157300482+nayanak241@users.noreply.github.com>
2024-12-17 22:14:48 +0530

    Create Saving taxes.cpp
    https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TAXSAVING

---
+++ Saving taxes.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int x,y;
+    cin>>x>>y;
+    int tax = x-y;
+    cout<<tax<<endl;
+}
+return 0;
+}

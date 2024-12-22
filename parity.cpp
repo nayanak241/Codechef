6a94a5e39a6f45c4177a8c10e1f5367ef08eca4d
nayanak241 <157300482+nayanak241@users.noreply.github.com>
2024-12-22 23:21:28 +0530

    Create parity.cpp
    https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PAR2?tab=statement

---
+++ parity.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+ int n;
+    cin>>n;
+    while(n--){
+        int n;
+        cin>>n;
+        if(n%2==0)
+        cout<<"Yes"<<endl;
+        else
+        cout<<"No"<<endl;
+    }
+    return 0;
+}
